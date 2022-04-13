ee4182a58ad0aa4e1385c62db30117bd1acebb38
w1nt3r-eth <dev@w1nt3r.eth>
2022-04-12 18:33:37 -0700

    Import SVG and Utils from Watchfaces.World

--- contracts/Renderer.sol
+++ contracts/Renderer.sol
@@ -1,8 +1,38 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
+import './SVG.sol';
+import './Utils.sol';
+
 contract Renderer {
     function render() external pure returns (string memory) {
-        return 'hello, world';
+        return
+            string.concat(
+                '<svg xmlns="http://www.w3.org/2000/svg" width="400" height="400" style="background:#000">',
+                string.concat(
+                    svg.rect(
+                        string.concat(
+                            svg.prop('fill', 'green'),
+                            svg.prop('x', '10'),
+                            svg.prop('y', '10'),
+                            svg.prop('width', utils.uint2str(100)),
+                            svg.prop('height', utils.uint2str(24))
+                        ),
+                        utils.NULL
+                    ),
+                    svg.text(
+                        string.concat(
+                            svg.prop('text-anchor', 'middle'),
+                            svg.prop('x', utils.uint2str(50)),
+                            svg.prop('y', '100'),
+                            svg.prop('font-size', '12'),
+                            svg.prop('fill', 'green')
+                        ),
+                        // _engraving
+                        svg.cdata('hello')
+                    )
+                ),
+                '</svg>'
+            );
     }
 }
