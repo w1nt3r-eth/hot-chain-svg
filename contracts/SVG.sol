97639a0a4b60bd66f338579262dfcdaf10865aeb
naomsa <djbrucelee@protonmail.com>
2022-04-15 18:21:42 -0300

    merged utils.sol into svg.sol

--- contracts/SVG.sol
+++ contracts/SVG.sol
@@ -1,10 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
-import './Utils.sol';
 
 // Core SVG utilitiy library which helps us construct
 // onchain SVG's with a simple, web-like API.
 library svg {
+    string constant internal NULL = "";
+
     /* MAIN ELEMENTS */
     function g(string memory _props, string memory _children)
         internal
@@ -98,11 +99,11 @@ library svg {
                 string.concat(
                     prop('stop-color', stopColor),
                     ' ',
-                    prop('offset', string.concat(utils.uint2str(offset), '%')),
+                    prop('offset', string.concat(uint2str(offset), '%')),
                     ' ',
                     _props
                 ),
-                utils.NULL
+                NULL
             );
     }
 
@@ -111,7 +112,7 @@ library svg {
         pure
         returns (string memory)
     {
-        return el('animateTransform', _props, utils.NULL);
+        return el('animateTransform', _props, NULL);
     }
 
     function image(string memory _href, string memory _props)
@@ -123,7 +124,7 @@ library svg {
             el(
                 'image',
                 string.concat(prop('href', _href), ' ', _props),
-                utils.NULL
+                NULL
             );
     }
 
@@ -155,5 +156,125 @@ library svg {
         returns (string memory)
     {
         return string.concat(_key, '=', '"', _val, '" ');
+    }
+
+    // formats a CSS variable line. includes a semicolon for formatting.
+    function setCssVar(string memory _key, string memory _val)
+        internal
+        pure
+        returns (string memory)
+    {
+        return string.concat('--', _key, ':', _val, ';');
+    }
+
+    // formats getting a css variable
+    function getCssVar(string memory _key)
+        internal
+        pure
+        returns (string memory)
+    {
+        return string.concat('var(--', _key, ')');
+    }
+
+    // formats getting a def URL
+    function getDefURL(string memory _id)
+        internal
+        pure
+        returns (string memory)
+    {
+        return string.concat('url(#', _id, ')');
+    }
+
+    // formats rgba white with a specified opacity / alpha
+    function white_a(uint256 _a) internal pure returns (string memory) {
+        return rgba(255, 255, 255, _a);
+    }
+
+    // formats rgba black with a specified opacity / alpha
+    function black_a(uint256 _a) internal pure returns (string memory) {
+        return rgba(0, 0, 0, _a);
+    }
+
+    // formats generic rgba color in css
+    function rgba(
+        uint256 _r,
+        uint256 _g,
+        uint256 _b,
+        uint256 _a
+    ) internal pure returns (string memory) {
+        string memory formattedA = _a < 100
+            ? string.concat('0.', uint2str(_a))
+            : '1';
+        return
+            string.concat(
+                'rgba(',
+                uint2str(_r),
+                ',',
+                uint2str(_g),
+                ',',
+                uint2str(_b),
+                ',',
+                formattedA,
+                ')'
+            );
+    }
+
+    // checks if two strings are equal
+    function stringsEqual(string memory _a, string memory _b)
+        internal
+        pure
+        returns (bool)
+    {
+        return
+            keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b));
+    }
+
+    // returns the length of a string in characters
+    function utfStringLength(string memory _str)
+        internal
+        pure
+        returns (uint256 length)
+    {
+        uint256 i = 0;
+        bytes memory string_rep = bytes(_str);
+
+        while (i < string_rep.length) {
+            if (string_rep[i] >> 7 == 0) i += 1;
+            else if (string_rep[i] >> 5 == bytes1(uint8(0x6))) i += 2;
+            else if (string_rep[i] >> 4 == bytes1(uint8(0xE))) i += 3;
+            else if (string_rep[i] >> 3 == bytes1(uint8(0x1E)))
+                i += 4;
+                //For safety
+            else i += 1;
+
+            length++;
+        }
+    }
+
+    // converts an unsigned integer to a string
+    function uint2str(uint256 _i)
+        internal
+        pure
+        returns (string memory _uintAsString)
+    {
+        if (_i == 0) {
+            return '0';
+        }
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len;
+        while (_i != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }
